834a7ab41a21a4fb79cd29ac34ae458e02e15850
Shayan Jamil <76084810+ShayanJamil519@users.noreply.github.com>
2023-02-06 10:07:29 +0500

    Create Wrap.cpp

---
+++ Wrap.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class LegacyClass {
+  public:
+    int oldMethod(int a, int b) {
+        return a + b;
+    }
+};
+
+class WrapperClass : public LegacyClass {
+  public:
+    // 1) Rename the old method you want to wrap
+    int oldMethodWrapped(int a, int b) {
+        return oldMethod(a, b);
+    }
+
+    // 2) Create a new method with the same name and signature as the old method
+    int oldMethod(int a, int b) {
+        // 4) Put the new logic before/after the other method call
+        std::cout << "Before call to wrapped method" << std::endl;
+        int result = oldMethodWrapped(a, b);
+        std::cout << "After call to wrapped method" << std::endl;
+        return result;
+    }
+};
+
+int main() {
+    WrapperClass obj;
+    int result = obj.oldMethod(2, 3);
+    std::cout << "Result: " << result << std::endl;
+    return 0;
+}
